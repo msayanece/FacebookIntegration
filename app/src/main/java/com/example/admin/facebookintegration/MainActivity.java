9cbd9c06955df10b3c3a0eea03435697e2d5f1c4
Admin <anshu.p@encoders.co.in>
2017-07-04 16:43:43 +0530

    get profile picture email DOB from fb

--- app/src/main/java/com/example/admin/facebookintegration/MainActivity.java
+++ app/src/main/java/com/example/admin/facebookintegration/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.admin.facebookintegration;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,7 +15,6 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
@@ -24,41 +22,57 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// tutorial: https://www.sitepoint.com/integrating-the-facebook-graph-api-in-android/
 // go to jre bin folder start cmd enter:
 //keytool -exportcert -alias androiddebugkey -keystore C:\Users\Admin\.android\debug.keystore | C:\Users\Admin\Desktop\s\bin\openssl.exe sha1 -binary | C:\Users\Admin\Desktop\s\bin\openssl.exe base64
 public class MainActivity extends AppCompatActivity {
 
     private String email;
+    private String name;
+    private String gender;
+    private String userId;
+    private URL profilePicture;
+    private String birthDay;
+    private CallbackManager mcallbackManager;
+    private LoginButton loginButton;
+    private ImageView markerImage;
+    private ImageView profileImageView;
+
+    /*
+    * setting callback for login results...
+    * called in onActivityResult
+    * called after result come back from facebook api
+    * if user press back button, onCancel() called
+    * if other error occurred, onError() called
+    * if user accepts the permission, onSuccess() called
+    */
     private FacebookCallback<LoginResult> mcallBack = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
-            AccessToken accessToken = loginResult.getAccessToken();
-            Profile profile = Profile.getCurrentProfile();
-            if (profile != null) {
-                putFacebookProfileInformation(profile);
-            }
+             /*
+            * request profile information and get response as a JSONObject
+            */
             GraphRequest request = GraphRequest.newMeRequest(
-                    loginResult.getAccessToken(),
+                    AccessToken.getCurrentAccessToken(),
                     new GraphRequest.GraphJSONObjectCallback() {
                         @Override
                         public void onCompleted(JSONObject object, GraphResponse response) {
-
                             Log.v("LoginActivity", response.toString());
-                            // Application code
-                            try {
-                                email = object.getString("email");
-//                                sendEmail();
-//                                String birthday = object.getString("birthday");
-//                                Toast.makeText(getApplicationContext(), "email: " + email + " and birthday: " + birthday, Toast.LENGTH_LONG).show();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                            // object has all the asked result in it
+                            putFacebookProfileInformation(object);
                         }
                     });
+            /*
+            * this is required for setting which data app wants to get
+            */
             Bundle parameters = new Bundle();
-            parameters.putString("fields", "id,name,email,gender,birthday");
+            parameters.putString("fields", "id, name, email, gender, birthday, first_name, last_name");
             request.setParameters(parameters);
             request.executeAsync();
+
             Toast.makeText(getApplicationContext(), "Log in Successful!", Toast.LENGTH_LONG).show();
         }
         @Override
@@ -71,43 +85,61 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Something went wrong, Log in Failed!", Toast.LENGTH_LONG).show();
         }
     };
-    private CallbackManager mcallbackManager;
-    private LoginButton loginButton;
-    private ImageView markerImage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // initialize FacebookSdk as early as possible
         FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_main);
-        markerImage = (ImageView) findViewById(R.id.image);
+        //create gallbackmanager object here
         mcallbackManager = CallbackManager.Factory.create();
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+        //initialize all the UI objects
+        profileImageView = (ImageView) findViewById(R.id.imageView);
+        loginButton = (LoginButton) findViewById(R.id.login_button);        //facebook login button
+
+        /* set the permission, user will see what are the data app will use
+         * these permission is a must if app wants anything except public info
+         * otherwise callback will only return id, name and gender (if available)
+         */
+        loginButton.setReadPermissions("email", "user_birthday","user_posts");
+        //set register callback for login button here
         loginButton.registerCallback(mcallbackManager, mcallBack);
     }
 
-    //putFacebookProfileInformation
-    private void putFacebookProfileInformation(Profile profile) {
-        String type = "signUp";
-        String accountType = "facebook";
-        String firstName = profile.getFirstName();
-        String lastName = profile.getLastName();
-        Uri image = profile.getProfilePictureUri(250,250);
-        markerImage.setVisibility(View.VISIBLE);
-        Picasso.with(MainActivity.this)
-                .load(image) /// if load from JSON .load(responseJSON.getString("image"))
-                .placeholder(R.drawable.noimage)
-                .error(R.drawable.noimage)
-                .into(markerImage);
-        Toast.makeText(this, "type="+type+", acc="+accountType+"name: "+profile.getFirstName()+" "+profile.getLastName(), Toast.LENGTH_LONG).show();
-        Log.e("fb: ", type+accountType+firstName+lastName);
+    /* putFacebookProfileInformation to local fields
+     * @param JSONObject returned from graphRequest
+     */
+    private void putFacebookProfileInformation(JSONObject object) {
+        try {
+            profileImageView.setVisibility(View.VISIBLE);
+            email = object.getString("email");
+            birthDay = object.getString("birthday");
+            name = object.getString("name");
+            gender = object.getString("gender");
+            userId = object.getString("id");
+            profilePicture = new URL("https://graph.facebook.com/" + userId + "/picture?width=250&height=250");
+            Picasso.with(MainActivity.this)
+                    .load(profilePicture.toString()) /// if load from JSON .load(responseJSON.getString("image"))
+                    .placeholder(R.drawable.noimage)
+                    .error(R.drawable.noimage)
+                    .into(profileImageView);
+            Log.d("sayan", "name: "+name+" and gender: "+gender+" and birthday: "+birthDay+" and email: "+email);
+        } catch (JSONException | MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
+    /*
+     * this method called after result comes from facebook login page
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        /*
+         * call mcallbackManager's onActivityResult method with the result data
+         */
         mcallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
